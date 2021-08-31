4e224eaa95aee1032f7454caf9f7d61acae25604
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-09-01 00:57:57 +0900

    cc

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,13 @@ module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'libraryapp',
+    htmlAttrs: {
+      lang: 'ko'
+    },
     meta: [
       { charset: 'utf-8' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { hid: 'description', name: 'description', content: '' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:title', property: 'og:title', content: 'library App' },
